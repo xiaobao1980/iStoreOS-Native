140afa75ad28386394789e5f87a0f467ee991936
xiaobao1980 <47268315+xiaobao1980@users.noreply.github.com>
2026-04-16 10:14:15 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -19,15 +19,6 @@ echo -e "\\ndefine Device/tvi_tvi3315a
 endef
 TARGET_DEVICES += tvi_tvi3315a" >> target/linux/rockchip/image/armv8.mk
 
-echo -e "\\ndefine Device/embedfire_lubancat-2n
-  DEVICE_VENDOR := EmbedFire
-  DEVICE_MODEL := LubanCat-2N
-  SOC := rk3568
-  UBOOT_DEVICE_NAME := lubancat-2n-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci
-endef
-TARGET_DEVICES += embedfire_lubancat-2n" >> target/linux/rockchip/image/armv8.mk
 
 # 替换package/boot/uboot-rockchip/Makefile
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-rockchip/Makefile
@@ -36,11 +27,9 @@ cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/Makefile package/boot/uboot-r
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3399-tvi3315a-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/tvi3315a-rk3399_defconfig package/boot/uboot-rockchip/src/configs/
-cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3568-lubancat-2n-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 
 # 复制dts到files/arch/arm64/boot/dts/rockchip
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/{rk3399.dtsi,rk3399-opp.dtsi,rk3399-tvi3315a.dts} target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
-cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3399/rk3568-lubancat-2.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
 # 添加dtb补丁到target/linux/rockchip/patches-6.6
 cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefile.patch target/linux/rockchip/patches-6.6/
@@ -52,6 +41,17 @@ cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefil
 # 移植RK3566示例，其他RK35xx可模仿
 # ============================================================================================================
 # 增加设备
+
+echo -e "\\ndefine Device/embedfire_lubancat-2n
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := LubanCat-2N
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := lubancat-2n-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci
+endef
+TARGET_DEVICES += embedfire_lubancat-2n" >> target/linux/rockchip/image/armv8.mk
+
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/legacy.mk target/linux/rockchip/image/legacy.mk
 
 # 复制dts与配置文件到package/boot/uboot-rockchip
@@ -59,10 +59,12 @@ cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts package/boot/ub
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/lubancat-2n-rk3568_defconfig package/boot/uboot-rockchip/src/configs/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3568-lubancat-2n-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 
 # 复制dts到target/linux/rockchip/dts/rk3568
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3568-lubancat-2.dts target/linux/rockchip/dts/rk3568/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3568-lubancat-2.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/
 
 # ============================================================================================================
 # RK35xx示例结束
