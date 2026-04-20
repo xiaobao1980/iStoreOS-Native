deb5160b4b5cf25b1b0d5c22780687354dbea7a6
xiaobao1980 <47268315+xiaobao1980@users.noreply.github.com>
2026-04-20 20:29:14 +0800

    Update diy-6.x-part2.sh

--- diy/diy-6.x-part2.sh
+++ diy/diy-6.x-part2.sh
@@ -40,15 +40,29 @@ cp -f $GITHUB_WORKSPACE/configfiles/patch/800-add-rk3399-tvi3315a-dtb-to-makefil
 # 移植RK3566示例，其他RK35xx可模仿
 # ============================================================================================================
 # 增加设备
+echo -e "\\ndefine Device/embedfire_lubancat-2n
+  DEVICE_VENDOR := embedfire
+  DEVICE_MODEL := lubancat-2n
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := lubancat-2n
+endef
+TARGET_DEVICES += embedfire_lubancat-2n" >> target/linux/rockchip/image/armv8.mk
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/legacy.mk target/linux/rockchip/image/legacy.mk
 
 # 复制dts与配置文件到package/boot/uboot-rockchip
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3568-lubancat-2n.dts package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3568-lubancat-2n-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
+cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/lubancat-2n-rk3568_defconfig package/boot/uboot-rockchip/src/configs/
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-roc-pc.dts package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/rk3566-station-m2-u-boot.dtsi package/boot/uboot-rockchip/src/arch/arm/dts/
 cp -f $GITHUB_WORKSPACE/configfiles/uboot-rockchip/station-m2-rk3566_defconfig package/boot/uboot-rockchip/src/configs/
 
 # 复制dts到target/linux/rockchip/dts/rk3568
 cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3566-jp-tvbox.dts target/linux/rockchip/dts/rk3568/
+cp -f $GITHUB_WORKSPACE/configfiles/dts/rk3568/rk3568-lubancat-2n.dts target/linux/rockchip/dts/rk3568/
+
+# 添加dtb补丁到target/linux/rockchip/patches-6.6
+cp -f $GITHUB_WORKSPACE/configfiles/patch/500-arm64-dts-rockchip-rk3588-Add-lubancat-2n.patch target/linux/rockchip/patches-6.6/
 # ============================================================================================================
 # RK35xx示例结束
 # ============================================================================================================
